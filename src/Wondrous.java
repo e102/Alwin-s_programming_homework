01a0f5cc18254617800e937d8c931afa1ca75279
Srdjan Miletic <SrdjaMiletic2@gmail.com>
2017-09-22 21:32:19 +0100

    renamed variables

--- src/Wondrous.java
+++ src/Wondrous.java
@@ -4,21 +4,23 @@
 
 public class Wondrous {
     public static void printHailStones(int input) {
-        int hailStoneSum = 0;
+        int numberOfSteps = 0;
         int originalNumber = input;
         while (input != 1) {
             if (input % 2 == 0) {
                 input = input / 2;
-                hailStoneSum++;
+                numberOfSteps++;
                 System.out.print(input + " ");
             } else {
                 input = (input * 3) + 1;
                 System.out.print(input + " ");
-                hailStoneSum++;
+                numberOfSteps++;
             }
         }
-        System.out.println("The hailstone sequence of " + originalNumber + " requires " + hailStoneSum + " steps ");
+        System.out.println("The hailstone sequence of " + originalNumber + " requires " + numberOfSteps + " steps ");
     }
+
+
 }
 
 
