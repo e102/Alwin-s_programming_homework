b10307df487ba89b55c0f7e122c140fd0b7ed262
Srdjan Miletic <SrdjaMiletic2@gmail.com>
2017-09-22 21:28:17 +0100

    Rationalized variables names and incrementation

--- src/Wondrous.java
+++ src/Wondrous.java
@@ -3,25 +3,25 @@
  */
 
 public class Wondrous {
-    public static boolean isEven(int x) {
-        return (x % 2 == 0);
+    private static boolean isEven(int number) {
+        return (number % 2 == 0);
     }
 
     public static void printHailStones(int n) {
-        int hailstoneSum = 0;
-        int orginialNumber = n;
+        int hailStoneSum = 0;
+        int originalNumber = n;
         while (n != 1) {
             if (isEven(n)) {
                 n = n / 2;
-                hailstoneSum = hailstoneSum + 1;
+                hailStoneSum++;
                 System.out.print(n + " ");
             } else {
                 n = (n * 3) + 1;
                 System.out.print(n + " ");
-                hailstoneSum = hailstoneSum + 1;
+                hailStoneSum++;
             }
         }
-        System.out.println("The hailstone sequence of " + orginialNumber + " requires " + hailstoneSum + " steps ");
+        System.out.println("The hailstone sequence of " + originalNumber + " requires " + hailStoneSum + " steps ");
     }
 }
 
